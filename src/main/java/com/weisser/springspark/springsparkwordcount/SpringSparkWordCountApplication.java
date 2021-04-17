83d1a9df45f4cb711ac8d4b63c8be37fd6aa8822
Oleg Vaiser <aly9681082@gmail.com>
2021-04-17 20:23:18 +0300

    init

---
+++ src/main/java/com/weisser/springspark/springsparkwordcount/SpringSparkWordCountApplication.java
@@ -0,0 +1,13 @@
+package com.weisser.springspark.springsparkwordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSparkWordCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSparkWordCountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weisser/springspark/springsparkwordcount/SpringSparkWordCountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weisser.springspark.springsparkwordcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSparkWordCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
